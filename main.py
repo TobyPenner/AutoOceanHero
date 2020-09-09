264eba543a789c3d720e0f3e68915fa556306b8b
Toby <770013@dpsk12.net>
2020-09-09 17:30:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import requests
+import threading
 from http.cookiejar import MozillaCookieJar
 
-count = 1
-url = "https://oceanhero.today/web?q=test"
-cj = MozillaCookieJar('cookies.txt')
-cj.load(ignore_expires=True)
-while True:
-    r = requests.get(url, cookies=cj)
-    print(str(r) + " " + str(count))
-    count += 1
+total_count = 1
+
+def main(threadname):
+    global total_count
+    url = "https://oceanhero.today/web?q=test"
+    cj = MozillaCookieJar('cookies.txt')
+    cj.load(ignore_expires=True)
+    while True:
+        requests.get(url, cookies=cj)
+        print(f"<{threadname}> {total_count}")
+        total_count += 1
+
+threads = list()
+for index in range(10):
+    thread = threading.Thread( target=main, args=(f"Thread{index+1}",))
+    threads.append(thread)
+    thread.start()
